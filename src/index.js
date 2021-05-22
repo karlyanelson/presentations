c94f2b55eb2199ab6ed9b99125228ba0193eeae0
Nelson, Karly <karly.nelson@hey.com>
2021-05-21 23:36:43 -0500

    add syntax highlighting css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Reveal from "reveal.js";
-import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
-import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.js";
+import Highlight from "reveal.js/plugin/highlight/highlight.js";
 
 let deck = new Reveal({
   controls: true,
