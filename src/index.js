97eee2e9ccce6939de10cc5ce30cfe12a521d10f
Nelson, Karly <karly.nelson@hey.com>
2021-05-21 19:00:51 -0500

    configure to be able to use markdown for presentations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
 
 let deck = new Reveal({
-  plugins: [Markdown],
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
+  plugins: [Markdown, Highlight],
 });
 deck.initialize();
